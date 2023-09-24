dd9164b799a6bdc46be9c7d71d94f8302188da6d
Brett Roberts <brett.roberts@live.ca>
2023-09-24 15:26:01 -0600

    Add categories api route file

---
+++ routes/categories-api.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router  = express.Router();
+
+const categoryQueries = require('../db/queries/categories');
+
+router.get('/', (req, res) => {
+  categoryQueries
+    .getCategories()
+    .then((categories) => {
+      res.send({ categories });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
+
+
+module.exports = router;
