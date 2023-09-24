9a61baedb4cf54f4bb1c82a3d2eb2e22b53c469d
Brett Roberts <brett.roberts@live.ca>
2023-09-24 15:27:23 -0600

    Add itemsForCategory api route file

---
+++ routes/itemsForCategory-api.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router  = express.Router();
+
+const itemsForCategoryQueries = require('../db/queries/itemsForCategory');
+
+router.get('/', (req, res) => {
+  itemsForCategoryQueries
+    .getItemsForCategory()
+    .then((items) => {
+      res.send({ items });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
+
+
+module.exports = { router };
