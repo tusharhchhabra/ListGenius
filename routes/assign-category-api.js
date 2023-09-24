1f6f61e4a6be881d26b62470cd1984dff7c7aa7b
Brett Roberts <brett.roberts@live.ca>
2023-09-24 15:26:29 -0600

    Add assign-category api route file

---
+++ routes/assign-category-api.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router  = express.Router();
+
+const assignCategoryQueries = require('../db/queries/assigncategory');
+
+router.get('/', (req, res) => {
+  assignCategoryQueries
+    .assignCategory()
+    .then((categories) => {
+      res.send({ categories });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
+
+router.post('/', (req, res) => {
+  // set user_id to cookie id
+  const user_id = req.session.user_id;
+  // validate cookie
+  if (!user_id) {
+    return res.send({ error: "error" });
+  }
+  // create new category from user input
+  const newCategory = req.body;
+  newCategory.owner_id = user_id;
+  categoryQueries
+    .addCategory(newCategory)
+    .then((category) => {
+      res.send(category);
+    })
+    .catch((err) => {
+      res.send(err.message);
+    });
+});
+
+module.exports = router;
