b88f76e697eb49996351156913ca9a8ab2d28454
Brett Roberts <brett.roberts@live.ca>
2023-09-25 21:03:15 -0600

    Add redirect to authenticate

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router  = express.Router();
 
-router.get('/', (req, res) => {});
+router.get('/', (req, res) => {
+  res.redirect('/login/1');
+});
 
 
 module.exports = router;
