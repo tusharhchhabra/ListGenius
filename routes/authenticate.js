9c17f0325201bf62d0900d1ccd840ff37d7f81ac
Brett Roberts <brett.roberts@live.ca>
2023-09-25 20:28:23 -0600

    Correct redirect url

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // localhost:3000/login/1
 router.get("/", (req, res) => {
   req.session.user_id = req.params.id;
-  res.redirect('/');
+  res.redirect('/api/categories');
 });
 
 module.exports = { router };
