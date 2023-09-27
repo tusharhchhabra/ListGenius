d9d884b823a77c869aee411a91ae7e17d24d81a7
Brett Roberts <brett.roberts@live.ca>
2023-09-27 14:08:30 -0600

    Complete refactor of AddItem. Add curl tests

--- routes/items-api.js
+++ routes/items-api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router  = express.Router();
 
 const itemsQueries = require('../db/queries/items');
-const { addCategory } = require('../db/queries/categories');
+const { addCategory, getCategoriesIDForUser } = require('../db/queries/categories');
 
 router.get('/', (req, res) => {
   const userId = req.cookies.user_id;
@@ -23,66 +23,55 @@ router.get('/', (req, res) => {
     });
 });
 
-router.post('/:id', (req, res) => {
 
-  const userId = req.cookies.user_id;
+// curl test for item with existing category: curl -X POST -H "Content-Type: application/json" -d '{"categoryId": "1", "category": "Movies", "userInput": "Saw"}' http://localhost:8080/api/items/1
 
-  if (!req.cookies || !req.cookies.user_id) {
-    return res.status(401).json({ message: 'Please sign up or log in to create lists.' });
-  }
 
-  // const userId = 1;
-  const newItem = req.body;
-  const newItemName = req.body.userInput;
+// curl test for item with no existing category: curl -X POST -H "Content-Type: application/json" -d '{"categoryId": "5", "category": "Travel", "userInput": "Japan"}' http://localhost:8080/api/items/1
 
-  let categoryId;
+router.post('/:id', async(req, res) => {
 
-  switch (req.body.category) {
-  case "Movies":
-    categoryId = 1;
-    break;
-  case "Eat":
-    categoryId = 2;
-    break;
-  case "Read":
-    categoryId = 3;
-    break;
-  case "Buy":
-    categoryId = 4;
-    break;
-  default:
-    addCategory(userId, req.body.category)
-      .then((result) => {
-        // Assuming addCategory returns the newly created category ID
-        categoryId = result;
-        newItem.owner_id = userId;
-        const categoryName = req.body.category;
+  // const userId = req.cookies.user_id;
 
-        // Get the next category ID
-        itemsQueries.findNextCategoryId()
-          .then((nextCategoryId) => {
-            categoryId = parseInt(nextCategoryId);
+  // if (!req.cookies || !req.cookies.user_id) {
+  //   return res.status(401).json({ message: 'Please sign up or log in to create lists.' });
+  // }
 
-            // Add the item with the new category ID
-            itemsQueries
-              .addItem(userId, categoryId, newItemName)
-              .then((newItem) => {
-                res.send(newItem);
-              })
-              .catch((err) => {
-                res.status(500).json({ error: err.message });
-              });
-          })
-          .catch((err) => {
-            res.status(500).json({ error: err.message });
+  const userId = 1;
+  const userInputCategoryId = parseInt(req.body.categoryId); // Convert to integer
+
+  getCategoriesIDForUser(userId)
+    .then((ids) => {
+      console.log("ids:", ids);
+      const categoryIdsArray = ids.map(catObj => catObj.categoryid); // Extract just the IDs
+
+      if (categoryIdsArray.includes(userInputCategoryId)) {
+        console.log("Category exists, adding item...");
+        return itemsQueries.addItem(userInputCategoryId, req.body.userInput);
+      } else {
+        console.log("Category does not exist, creating category...");
+        return addCategory(userId, req.body.category)
+          .then((result) => {
+            const newCategoryId = result.id;  // Extracting the actual ID
+            console.log("Created new category with ID:", newCategoryId);
+            console.log("Adding item to the new category...");
+            return itemsQueries.addItem(newCategoryId, req.body.userInput);
           });
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
-    break;
-  }
+      }
+    })
+    .then((newItem) => {
+      res.send(newItem);
+    })
+    .catch((err) => {
+      console.error("Error while processing:", err);
+      res.status(500).json({ error: err.message });
+    });
 });
+
+
+// Update Item
+// cur test: curl -X PATCH -H "Content-Type: application/json" -d '{"name": "Updated Item Name"}' http://localhost:8080/api/items/5
+
 
 router.patch('/:id', (req, res) => {
 
@@ -108,6 +97,9 @@ router.patch('/:id', (req, res) => {
       res.send(err.message);
     });
 });
+
+// Delete Item
+// curl test: curl -X DELETE http://localhost:8080/api/items/5
 
 router.delete('/:id', (req, res) => {
 
