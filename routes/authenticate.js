374f8cf908f0fdc38af132b048d4ffc8984d0c82
Brett Roberts <brett.roberts@live.ca>
2023-09-26 21:48:15 -0600

    Quick fix cookies

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-// localhost:3000/login/1
-router.get("/", (req, res) => {
-  req.session.user_id = req.params.id;
+// Define a route with a parameter named 'id'
+// This will match URLs like localhost:3000/login/1
+router.get("/:id", (req, res) => {
+  if (req.params.id) {
+    res.cookie('user_id', 1);
+  }
+
+  // Redirect to another route
   res.redirect('/api/categories');
 });
 
-module.exports = { router };
+
+module.exports = router;
