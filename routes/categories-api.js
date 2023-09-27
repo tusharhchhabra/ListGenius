d6bb8211e4e23c0a1a82d416443c98092c9847df
Brett Roberts <brett.roberts@live.ca>
2023-09-26 21:49:57 -0600

    Fix cookie issue with all routes. Add POST addCategory, PATCH updateCategory, DELETE delete category routes

--- routes/categories-api.js
+++ routes/categories-api.js
@@ -1,21 +1,44 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 const categoryQueries = require('../db/queries/categories');
+const itemsForCategoryQueries = require('../db/queries/items');
 
+
+
+// Get all categories from a user
 router.get('/', (req, res) => {
+  const userId = req.cookies.user_id;
 
-  if (!req.session || !req.session.user_id) {
-    window.alert('Please sign up or log in to create lists.');
-    return res.redirect('/home');
+  if (!req.cookies || !req.cookies.user_id) {
+    return res.status(401).json({ message: 'Please sign up or log in to create lists.' });
   }
 
-  const userId = req.session.user_id;
-
   categoryQueries
-    .getUserCategories(userId)
+    .getCategoriesForUser(userId)
     .then((categories) => {
-      res.send({ categories });
+      res.json({ categories });
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+// Get all items for a category from a user
+router.get('/:id', (req, res) => {
+
+  const userId = req.cookies.user_id;
+  const categoryId = req.params.id;
+  console.log("req.params.id", req.params.id);
+
+  if (!req.cookies || !req.cookies.user_id) {
+    return res.status(401).json({ message: 'Please sign up or log in to create lists.' });
+  }
+
+  itemsForCategoryQueries
+    .getItemsForCategory(userId, categoryId)
+    .then((items) => {
+      res.send({ items });
     })
     .catch(err => {
       res
@@ -24,5 +47,81 @@ router.get('/', (req, res) => {
     });
 });
 
+// Add category
+
+router.post('/', (req, res) => {
+
+  const userId = 1;
+  req.cookies.user_id;
+
+  // if (!req.cookies || !req.cookies.user_id) {
+  //   return res.status(401).json({ message: 'Please sign up or log in to create lists.' });
+  // }
+
+  const categoryName = req.body.name;
+  const owner_id = userId;
+  // const categoryId = ? I think this is auto added?
+
+
+  categoryQueries
+    .addCategory(owner_id, categoryName)
+    .then((category) => {
+      res.send(category);
+    })
+    .catch((err) => {
+      res.send(err.message);
+    });
+});
+
+router.patch('/:id', (req, res) => {
+
+  const userId = req.cookies.user_id;
+
+  if (!req.cookies || !req.cookies.user_id) {
+    return res.status(401).json({ message: 'Please sign up or log in to create lists.' });
+  }
+
+  const categoryName = req.body.name;
+  // const owner_id = userId;
+  const categoryId = req.params.id;
+
+
+  categoryQueries
+    .updateCategory(categoryId, categoryName)
+    .then((category) => {
+      res.send(category);
+    })
+    .catch((err) => {
+      res.send(err.message);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+
+  // const userId = req.cookies.user_id;
+
+  if (!req.cookies || !req.cookies.user_id) {
+    return res.status(401).json({ message: 'Please sign up or log in to create lists.' });
+  }
+
+  // const categoryName = req.body.name;
+  // const owner_id = userId;
+  const categoryId = req.params.id;
+
+
+  categoryQueries
+    .deleteCategory(categoryId)
+    .then((category) => {
+      res.send(category);
+    })
+    .catch((err) => {
+      res.send(err.message);
+    });
+});
+
+
+
+
 
 module.exports = router;
+
