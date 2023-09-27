52e8f73f30ab45b50f3ca7138e5ec589b9f3c466
Brett Roberts <brett.roberts@live.ca>
2023-09-26 21:55:15 -0600

    change queries to return more specific results. Add a new query used in addItems to find the next categoryId

--- db/queries/items.js
+++ db/queries/items.js
@@ -3,11 +3,12 @@ const db = require('../connection');
 /* Fetch Items related to a specific category */
 
 const getItemsForCategory = (userId, categoryId) => {
-  const query = `SELECT *
+  const query = `SELECT items.name
   FROM items
   JOIN users
   ON users.id = owner_id
-  WHERE users.id = $1 and categories_id = $2`
+  WHERE users.id = $1 and categories_id = $2
+  GROUP BY items.name;`;
   return db.query(query,[userId, categoryId])
     .then(data => {
       return data.rows;
@@ -20,14 +21,14 @@ const getItemsForCategory = (userId, categoryId) => {
 /* List all items from a user */
 
 const getAllItemsOfUser = (userId) => {
-  const query = `SELECT *
+  const query = `SELECT items.name
   FROM items
   JOIN users
   ON users.id = owner_id
-  WHERE users.id = $1;`
+  WHERE users.id = $1;`;
   return db.query(query,[userId])
     .then(data => {
-      return data.rows[0];
+      return data.rows;
     })
     .catch((err) => {
       console.log(err.message);
@@ -36,7 +37,7 @@ const getAllItemsOfUser = (userId) => {
 
 const addItem = (owner_id, categoryId, name) => {
   const query = `INSERT INTO items(owner_id, categories_id, name, created_at)
-  VALUES ($1, $2, $3) RETURNING id;`
+  VALUES ($1, $2, $3, Now()) RETURNING id;`;
   return db.query(query, [owner_id, categoryId, name])
     .then(data => {
       return data.rows[0];
@@ -67,6 +68,19 @@ const updateItem = (itemId, name) => {
     });
 };
 
+const findNextCategoryId = () => {
+  const query = `SELECT categories_id FROM items ORDER BY categories_id DESC LIMIT 1;`;
+  return db.query(query)
+    .then(data => {
+      const highestCategoryId = data.rows[0].categories_id; // Access the 'categories_id' column
+      return parseInt(highestCategoryId) + 1; // Convert to integer and add 1
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+
 
 
 module.exports = {
@@ -74,5 +88,6 @@ module.exports = {
   getAllItemsOfUser,
   addItem,
   deleteItem,
-  updateItem
+  updateItem,
+  findNextCategoryId
 };
