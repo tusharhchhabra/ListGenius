42fcdf7c472c91dc57a1d5130d6b46c331cb6a58
Tushar Chhabra <tusharhchhabra@gmail.com>
2023-09-28 16:20:46 -0700

    Fix profile update across header and inputs

--- public/components/profile.js
+++ public/components/profile.js
@@ -9,7 +9,7 @@ $(() => {
       <span class="content-heading">Profile</span>
       <div id="profile-data">
         <input id="name" placeholder="Name" value="${user.name}">
-        <input id="email" placeholder="Email Address" value="${user.email_adress}">
+        <input id="email" placeholder="Email Address" value="${user.email_address}">
       </div>
       <button class="save-profile-button">Save</button>
     </div>`;
@@ -22,8 +22,11 @@ $(() => {
   }
   window.userProfile.update = updateProfileView;
 
-  $('.profile-data').on('input', function() {
-    if (this.value.length > 0) {
+  $main.on("input", "#name, #email", function() {
+    const currentName = $('#name').val();
+    const currentEmail = $('#email').val();
+
+    if (currentName !== window.userProfile.name || currentEmail !== window.userProfile.email_address) {
       $('.save-profile-button').show();
     } else {
       $('.save-profile-button').hide();
@@ -33,6 +36,12 @@ $(() => {
   $main.on("click", ".save-profile-button", function() {
     const userName = $main.find('#name').val();
     const userEmail = $main.find('#email').val();
-    updateUser({ userName, userEmail });
+    updateUser({ userName, userEmail })
+      .then(user => {
+        if (user) {
+          currentUser = user;
+          window.header.update(user)
+        }
+      });
   });
 });
