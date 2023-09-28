f59bf36f2cd21d73af40d769a4c5364a816ab23b
Brett Roberts <brett.roberts@live.ca>
2023-09-27 18:08:03 -0600

    Add missing AJAX requests

--- public/libraries/network.js
+++ public/libraries/network.js
@@ -1,41 +1,52 @@
 // GET / UPDATE User Profile
-function getUserDetails() {
-  console.log(getUserDetails);
+function getUser(data) {
   return $.ajax({
     method: "GET",
-    url: "/api/users"
+    url: "/api/users/:id",
+    data
   });
 }
 
-function updateUserDetails(data) {
+function updateUser(data) {
   return $.ajax({
     method: "POST",
-    url: "/api/profile",
+    url: "/api/users/:id",
     data
   });
 }
 
 // GET ALL CATEGORIES w/ ITEM COUNT
-function getUserCategories() {
+function getCategoriesForUsers(data) {
   return $.ajax({
     method: "GET",
-    url: "/api/categories"
+    url: "/api/categories",
+    data
   });
 }
 
-
-// GET ALL CATEGORIES / UPDATE CATGEORY
-function assignCategory() {
-  return $.ajax({
-    method: "GET",
-    url: "/api/assigncategory"
-  });
-}
-
-function assignCategory(data) {
+// Add category
+function addCategory(data) {
   return $.ajax({
     method: "POST",
-    url: "/api/assigncategory",
+    url: "/api/categories/:id",
+    data
+  });
+}
+
+
+// UPDATE CATGEORY
+function updateCategory(data) {
+  return $.ajax({
+    method: "POST",
+    url: "/api/categories/:id",
+    data
+  });
+}
+
+function deleteCategory(data) {
+  return $.ajax({
+    method: "POST",
+    url: "/api/categories/:id",
     data
   });
 }
@@ -45,24 +56,47 @@ function assignCategory(data) {
 function getItemsForCategory() {
   return $.ajax({
     method: "GET",
-    url: "/api/itemsforcategory"
+    url: "/api/categories/:id"
   });
 }
 
-// GET ALL ITMES + ADD ITEM
-function getItems() {
-  return $.ajax({
-    method: "GET",
-    url: "/api/items"
-  });
-}
+// // GET ALL ITMES + ADD ITEM
+// function getItems() {
+//   return $.ajax({
+//     method: "GET",
+//     url: "/api/items"
+//   });
+// }
 
-function addItems(data) {
+function addItem(data) {
   return $.ajax({
     method: "POST",
-    url: "/api/items",
+    url: "/api/items/",
     data
   });
 }
 
+function deleteItem(data) {
+  return $.ajax({
+    method: "POST",
+    url: "/api/items/:id",
+    data
+  });
+}
+
+function updateItemName(data) {
+  return $.ajax({
+    method: "POST",
+    url: "/api/items/:id",
+    data
+  });
+}
+
+function updateItemCategory(data) {
+  return $.ajax({
+    method: "POST",
+    url: "/api/items/:id",
+    data
+  });
+}
 
