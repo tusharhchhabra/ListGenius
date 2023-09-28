ccf171e70946c73c53bcf0c99d3b3f09bdec8392
Brett Roberts <brett.roberts@live.ca>
2023-09-28 12:36:01 -0600

    Add more sample data

--- public/exampleData/exampleItems.js
+++ public/exampleData/exampleItems.js
@@ -19,5 +19,117 @@ const itemsEx = [
     categories_id: 1,
     name: 'Lord of the Rings',
     created_at: '2023-08-15T14:22:30Z'
+  },
+  {
+    id: 2,
+    owner_id: 101,
+    categories_id: 1,
+    name: 'Addams Family Values',
+    created_at: '2023-09-20T08:30:45Z',
+  },
+  {
+    id: 3,
+    owner_id: 101,
+    categories_id: 1,
+    name: 'Lord of the Rings',
+    created_at: '2023-08-15T14:22:30Z'
+  },
+  {
+    id: 2,
+    owner_id: 101,
+    categories_id: 1,
+    name: 'Addams Family Values',
+    created_at: '2023-09-20T08:30:45Z',
+  },
+  {
+    id: 3,
+    owner_id: 101,
+    categories_id: 1,
+    name: 'Lord of the Rings',
+    created_at: '2023-08-15T14:22:30Z'
+  },
+  {
+    id: 2,
+    owner_id: 101,
+    categories_id: 1,
+    name: 'Addams Family Values',
+    created_at: '2023-09-20T08:30:45Z',
+  },
+  {
+    id: 3,
+    owner_id: 101,
+    categories_id: 1,
+    name: 'Lord of the Rings',
+    created_at: '2023-08-15T14:22:30Z'
+  },
+  {
+    id: 2,
+    owner_id: 101,
+    categories_id: 1,
+    name: 'Addams Family Values',
+    created_at: '2023-09-20T08:30:45Z',
+  },
+  {
+    id: 3,
+    owner_id: 101,
+    categories_id: 1,
+    name: 'Lord of the Rings',
+    created_at: '2023-08-15T14:22:30Z'
+  },
+  {
+    id: 2,
+    owner_id: 101,
+    categories_id: 1,
+    name: 'Addams Family Values',
+    created_at: '2023-09-20T08:30:45Z',
+  },
+  {
+    id: 3,
+    owner_id: 101,
+    categories_id: 1,
+    name: 'Lord of the Rings',
+    created_at: '2023-08-15T14:22:30Z'
+  },
+  {
+    id: 3,
+    owner_id: 101,
+    categories_id: 1,
+    name: 'Lord of the Rings',
+    created_at: '2023-08-15T14:22:30Z'
+  },
+  {
+    id: 2,
+    owner_id: 101,
+    categories_id: 1,
+    name: 'Addams Family Values',
+    created_at: '2023-09-20T08:30:45Z',
+  },
+  {
+    id: 3,
+    owner_id: 101,
+    categories_id: 1,
+    name: 'Lord of the Rings',
+    created_at: '2023-08-15T14:22:30Z'
+  },
+  {
+    id: 3,
+    owner_id: 101,
+    categories_id: 1,
+    name: 'Lord of the Rings',
+    created_at: '2023-08-15T14:22:30Z'
+  },
+  {
+    id: 2,
+    owner_id: 101,
+    categories_id: 1,
+    name: 'Addams Family Values',
+    created_at: '2023-09-20T08:30:45Z',
+  },
+  {
+    id: 3,
+    owner_id: 101,
+    categories_id: 1,
+    name: 'Lord of the Rings',
+    created_at: '2023-08-15T14:22:30Z'
   }
 ];
